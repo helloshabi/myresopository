c7a021ce606ada6ffafd1dc1054aa56568bf91aa
zhj <zhj@zhj.com>
2018-12-16 20:59:59 +0800

    test第二次for循环

--- ems/src/test/java/com/baizhi/EmsTest.java
+++ ems/src/test/java/com/baizhi/EmsTest.java
@@ -15,5 +15,8 @@ public class EmsTest {
         for (int i = 0; i <10 ; i++) {
             System.out.println(i);
         }
+        for (int i = 0; i <20 ; i++) {
+            System.out.println(i);
+        }
     }
 }
