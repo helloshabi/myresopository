4687af2ffddfad98f13a3bba8a3b42c2a8786a9b
zhj <zhj@zhj.com>
2018-12-16 20:45:36 +0800

    提交一个新建项目

---
+++ ems/src/test/java/com/baizhi/EmsTest.java
@@ -0,0 +1,19 @@
+package com.baizhi;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Application.class)
+public class EmsTest {
+    @Test
+    public void test(){
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(i);
+        }
+    }
+}
