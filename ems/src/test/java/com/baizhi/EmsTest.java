788ac42d3c329e703f507613d2fe25635532be60
zhj <zhj@zhj.com>
2018-12-17 14:01:22 +0800

    传到我的远程仓库

--- ems/src/test/java/com/baizhi/EmsTest.java
+++ ems/src/test/java/com/baizhi/EmsTest.java
@@ -6,6 +6,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes=Application.class)
 public class EmsTest {
@@ -13,10 +16,20 @@ public class EmsTest {
     public void test(){
 
         for (int i = 0; i <10 ; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
+        System.out.println();
         for (int i = 0; i <20 ; i++) {
-            System.out.println(i);
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        String aa="11";
+        String bb="22";
+        List list =new ArrayList<String>();
+        list.add(aa);
+        list.add(bb);
+        for (Object o : list) {
+            System.out.println(o);
         }
     }
 }
